bc5bfe2fd5f6dcef14d3aeee88e0430de821de54
Natalia Shport <shport.natalia@gmail.com>
2020-04-08 23:54:48 -0700

    First commit

---
+++ src/build/precache-manifest.c422ac6e2a791912f535de6421765c6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e45d1aaf6bf186f365ec2e6be6ffd1c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c17a995e17f433388e81",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "137a218b11a19bb1f92f",
+    "url": "/static/js/2.bc216925.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bc216925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c17a995e17f433388e81",
+    "url": "/static/js/main.e99a3331.chunk.js"
+  },
+  {
+    "revision": "209d7516a220ea251298",
+    "url": "/static/js/runtime-main.cc5c5978.js"
+  }
+]);
